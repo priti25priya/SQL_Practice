3667c3e1117dd054cbc3714f00aad26e910d505f
Priti Priya <priti.priya@diggibyte.com>
2024-10-03 16:48:41 +0530

    Performing common table expression

---
+++ CTE.sql
@@ -0,0 +1,24 @@
+-- CTE(COMMON TABLE EXPRESSION):
+CREATE TABLE CUSTOMERS (
+   ID INT NOT NULL,
+   NAME VARCHAR (20) NOT NULL,
+   AGE INT NOT NULL,
+   ADDRESS CHAR (25),
+   SALARY DECIMAL (18, 2),       
+   PRIMARY KEY (ID)
+);
+
+INSERT INTO CUSTOMERS VALUES 
+(1, 'Ramesh', 32, 'Ahmedabad', 2000.00 ),
+(2, 'Khilan', 25, 'Delhi', 1500.00 ),
+(3, 'Kaushik', 23, 'Kota', 2000.00 ),
+(4, 'Chaitali', 25, 'Mumbai', 6500.00 ),
+(5, 'Hardik', 27, 'Bhopal', 8500.00 ),
+(6, 'Komal', 22, 'Hyderabad', 4500.00 ),
+(7, 'Muffy', 24, 'Indore', 10000.00 );
+
+-- Creating a Common Table Expression (CTE) named CUSTOMER_AGE that selects all customers with an age of 23. 
+-- We are then retrieving the ID, NAME, and AGE of these customers from the CTE.
+WITH CUSTOMER_AGE AS (
+SELECT * FROM customers WHERE AGE = 23)
+SELECT ID, NAME, AGE FROM CUSTOMER_AGE;
